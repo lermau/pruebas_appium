610cebe0b30382328c5a4e5ad829b99f3f155fd9
Mauricio Valencia <mauriciovl@valerma.com>
2023-10-05 18:09:49 -0600

    Se agrega el envío de cedulas sin realizar encuestas

--- comparadiferencias2.py
+++ comparadiferencias2.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 fila_inicio = 8
-# Carga los dos archivos de Excel en DataFrames
-df1 = pd.read_excel('REPORTE_FINAL_ACTUALIZACION.xlsx', skiprows=range(1, fila_inicio))
-df2 = pd.read_excel('REPORTE_FIN_ACT.xlsx', skiprows=range(1, fila_inicio))
+fila_encabezado = 2
+# Carga el archivo de Excel en un DataFrame y usa la fila_encabezado como encabezado
+df1 = pd.read_excel('REPORTE_FINAL_ACTUALIZACION.xlsx', skiprows=range(1, fila_inicio), header=fila_encabezado-1)
+df2 = pd.read_excel('REPORTE_FIN_ACT.xlsx', skiprows=range(1, fila_inicio), header=fila_encabezado-1)
 
 # Compara los DataFrames y obtén las diferencias
 diferencias = df1.compare(df2)
@@ -20,4 +21,7 @@ if not diferencias.empty:
     print(f"\nSe encontraron {num_diferencias} diferencias. \nLas diferencias se han guardado en 'diferencias.txt'.")
 else:
     print("No se encontraron diferencias entre los archivos.")
+
+
+
 
